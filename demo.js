fc21a5a99e75a598e5f5814d73ea232b98df5682
Louis Tiede <uutqh@student.kit.edu>
2022-01-18 12:30:10 +0100

    Added koeri location

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 /****************** GPS **************************/
 var x = document.getElementById("p_geoloc");
 
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -12,6 +13,9 @@ function getLocation() {
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;	
+
+	document.getElementById("#distance").innerHTML = "distance: " + getDistanceFromLatLonInKm(position.coords.latitude, position.coords.longitude,
+		koeriLatitude, koeriLongitude);
 }
 
 function showError(error) {
@@ -31,6 +35,31 @@ function showError(error) {
     }
 }
 /******************************************************/
+
+/* GPS-Values for [koeri]werk */
+
+static var koeriLatitude = 49.011636;
+static var koeriLongitude = 8.416395;
+
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	var dLon = deg2rad(lon2-lon1); 
+	var a = 
+	  Math.sin(dLat/2) * Math.sin(dLat/2) +
+	  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	  Math.sin(dLon/2) * Math.sin(dLon/2)
+	  ; 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
+  }
+  
+  function deg2rad(deg) {
+	return deg * (Math.PI/180)
+  }
+
 
 
 /* Viewport Breite */
